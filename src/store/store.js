31e47c360e5ce451f215453763b18e27fb262d74
Bob Cunningham <bobchm@gmail.com>
2022-08-04 10:58:20 -0400

    created basic Redux store and root-reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,12 @@
-import { compose, createStore, applyMiddleware } from "redux";
+import { compose, applyMiddleware } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+// root-redcucer
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
