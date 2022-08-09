43a89df7af09b75a2332ab3685fd55eb161fbaac
Bob Cunningham <bobchm@gmail.com>
2022-08-08 21:22:05 -0400

    memoized cart selectors

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -1,2 +1,13 @@
-export const selectIsCartOpen = (state) => state.cart.isCartOpen;
-export const selectCartItems = (state) => state.cart.cartItems;
+import { createSelector } from "reselect";
+
+const selectCartReducer = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCartReducer],
+    (cart) => cart.cartItems
+);
+
+export const selectIsCartOpen = createSelector(
+    [selectCartReducer],
+    (cart) => cart.isCartOpen
+);
