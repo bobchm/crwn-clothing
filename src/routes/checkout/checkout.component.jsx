2a4e8f8139c34465acfc3b53323709cc35c0f7fb
Bob Cunningham <bobchm@gmail.com>
2022-08-11 20:35:00 -0400

    added support for Stripe payments

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { selectCartItems } from "../../store/cart/cart.selector";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
@@ -25,6 +26,7 @@ const Checkout = () => {
                 <CheckoutItem key={item.id} item={item} />
             ))}
             <span className="total">TOTAL: ${totalPrice()}</span>
+            <PaymentForm />
         </div>
     );
 };
