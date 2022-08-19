af530f7726a04cf5deadf410c64f6bbd19887ac2
Bob Cunningham <bobchm@gmail.com>
2022-08-19 13:38:40 -0400

    styling to help with mobile

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./routes/home/home.component.jsx";
 import Authentication from "./routes/authentication/authentication-component.jsx";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component.jsx";
+import { GlobalStyle } from "./global.styles.js";
 import { checkUserSession } from "./store/user/user.action";
 import { fetchCategoriesStart } from "./store/categories/category.action";
 
@@ -20,14 +21,17 @@ const App = () => {
     }, []);
 
     return (
-        <Routes>
-            <Route path="/" element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path="shop/*" element={<Shop />} />
-                <Route path="auth" element={<Authentication />} />
-                <Route path="checkout" element={<Checkout />} />
-            </Route>
-        </Routes>
+        <div>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route path="shop/*" element={<Shop />} />
+                    <Route path="auth" element={<Authentication />} />
+                    <Route path="checkout" element={<Checkout />} />
+                </Route>
+            </Routes>
+        </div>
     );
 };
 
